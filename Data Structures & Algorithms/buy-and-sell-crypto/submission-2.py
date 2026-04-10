0a922c46472ba62f26adcf64ce52aafe72381db7
neta-shami <94456735+neta-shami@users.noreply.github.com>
2026-04-10 17:14:31 +0300

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy , sell = 0,1
+        maxProfit = 0
+
+        while sell <len(prices):
+            if prices[buy] < prices[sell]: #we have profit
+                profit = prices[sell] - prices[buy]
+                maxProfit = max(maxProfit , profit)
+            else: #we fount lower change to buy 
+                buy = sell
+            sell+=1 # as long as we didn't find better time to buy we keep scaninng for time to sell 
+        return maxProfit
