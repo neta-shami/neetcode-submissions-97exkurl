2a9423d71079cd4e31380bdfaf71172c9ebfdd30
neta-shami <94456735+neta-shami@users.noreply.github.com>
2026-04-11 10:15:18 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+        closeToOpenMap = {
+            "}" : "{" ,
+            ")" : "(" ,
+            "]" : "["
+        }
+
+        for ch in s:
+            if ch in closeToOpenMap.values(): #if ch is openning  bracket push to stack
+                stack.append(ch)
+            else:
+                if stack and closeToOpenMap[ch] == stack[-1]: #stack is not empty and the openning for ch is in the to-p of the satck
+                    stack.pop()
+                else:
+                    return False 
+        
+        return True if not stack else False
+
+
+            
+
+
+
+
