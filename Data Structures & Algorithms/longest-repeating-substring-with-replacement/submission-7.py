ec191a17a5f6a57bb05a88796565e410c4f20daf
neta-shami <94456735+neta-shami@users.noreply.github.com>
2026-04-14 11:52:47 +0300

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,52 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        n = len(s)
+
+        if n==1:
+            return 1
+
+        counter = defaultdict(int)
+        maxLen = 0
+        i , j = 0,0
+
+
+        while j<n:
+            counter[s[j]] +=1
+            m = getMaxLetter(counter)
+            if j-i+1-m > k: 
+                maxLen= max(j-i, maxLen)
+                counter[s[i]] -=1
+                i+=1
+            j+=1
+        
+        maxLen= max(j-i , maxLen)    
+        return maxLen
+
+def getMaxLetter(counter):
+    m = 0
+    for i in counter:
+        m = max(m , counter[i])
+    return m
+
+'''
+        
+        counter = defaultdict(int)
+        i , j = n-1, n-1
+        currLetter = s[i]
+        
+        while j>=0:
+            counter[s[j]] +=1
+            if i-j+1-getMaxLetter(counter) > k: #skips<= k
+                maxLen= max(i-j, maxLen)
+                while s[i] == currLetter:
+                    counter[s[i]] -=1
+                    i-=1
+                currLetter = s[i]
+            j-=1
+        
+        maxLen= max(i-j , maxLen)  
+        '''
+            
+
+        
