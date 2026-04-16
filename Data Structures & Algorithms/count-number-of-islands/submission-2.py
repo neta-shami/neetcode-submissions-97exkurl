7c26d4d2aa85ae6193bfd9221cbcf466551c5df1
neta-shami <94456735+neta-shami@users.noreply.github.com>
2026-04-16 22:55:12 +0300

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def color(i,j):
+            if i<0 or i>=n or j<0 or j>=m:
+                return
+            if grid[i][j] == "1":
+                grid[i][j] = "0"
+                for x,y in directions:
+                    color(i+x, y+j)
+                
+            return 
+
+        islands = 0
+        n = len(grid)
+        m = len(grid[0])
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    islands+=1
+                    color(i,j)
+        
+        return islands
+        
+        
+            
+
